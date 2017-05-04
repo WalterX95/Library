56a348d3f39dc7fe0cfc87d36be97a472dbd7106
WalterX95 <walter_1995@hotmail.it>
2017-05-04 11:31:50 +0200

    Create system.php

---
+++ system.php
@@ -0,0 +1,19 @@
+<?php
+
+/* This is system library for ElevenDots 
+  Library for GNU PUBLIC LICENSE v2.0
+  
+*/
+
+
+  class Database {
+  
+    public function __construct() {
+    
+      
+    
+    }
+  
+  }
+
+?>
